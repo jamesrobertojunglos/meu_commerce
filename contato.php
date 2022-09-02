de92cbdde1fb13085f79146147a6165c4ac701f7
jamesrobertojunglos <jamesrobertojunglos75@gmail.com>
2022-09-01 23:17:03 -0300

    Testando a Class btn

---
+++ contato.php
@@ -0,0 +1,9 @@
+Endereço, Telefone, E_mail, Redes sociais, Chat, 0800....
+<br>
+
+<?php
+echo "<h1>".$_GET['codigo']."</h1>";
+echo "<h1>".$_GET['nome']."</h1>";
+echo "<h1>".$_GET['sobrenome']."</h1>";
+
+?>
