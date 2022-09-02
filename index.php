289a8620bcc1a3d79cd4e7a2f5df46b34ce10518
jamesrobertojunglos <jamesrobertojunglos75@gmail.com>
2022-09-02 01:12:11 -0300

    Criando a table

--- index.php
+++ index.php
@@ -7,7 +7,27 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
   </head>
   <body>
-    <a href="a" class="btn btn-danger">Teste</a>
+    <div class="container">
+       <div class = "row">
+          <div class="col-12 bg-secondary">Cabeçalho</div>   
+    </div>
+
+        <div class = "row">
+            <div class="col-2 bg-primary">
+                Menu 
+            </div>
+    
+            <div class="col-10">
+                Corpo
+            </div>
+        </div>
+
+        <div class ="row">
+            <div class="col-12 bg-info text-align-center">rodapé</div>
+        </div>
+
+    </div>
+    
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
   </body>
 </html>
