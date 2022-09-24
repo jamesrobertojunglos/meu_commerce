bab9a18a72b6daf071e64b809095595ac7ee2f2e
jamesrobertojunglos <jamesrobertojunglos75@gmail.com>
2022-09-24 19:10:41 -0300

    consegui descobrir, estava digitando o comando errado no menu_categorias.php

--- home.php
+++ home.php
@@ -12,25 +12,35 @@
     <body>
         <div class="container">
             <div class="row">
-                <div class="col">Banner</div>
+                <div class="col">
+                    Banner
+                </div>
             </div>
             <div class="row">
-                <div class="col">Menu horizontal</div>
+                <div class="col">
+                    Menu horizontal
+                </div>
             </div>
             <div class="row">
                 <div class="col-2">
-                    <? include "menu_categorias.php"; ?>
+                    <?php include "menu_categorias.php"; ?>
                 </div>
-                <div class="col-10">
-                    produtos
+                <div>
+                    <div class="row">
+                        <div class="col-2">
+                            menu vertical
+                        </div>
+                        <div class="col-10">
+                            produtos
+                        </div>
+                    </div>
+                    <div class="row" style="background-color:aqua">@meu_commerce 2022</div>
                 </div>
-            </div>
-            <div class="row" style="background-color:aqua">@meu_commerce 2022</div>
-        </div>
 
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js"
-            integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous">
-        </script>
+                <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js"
+                    integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8"
+                    crossorigin="anonymous">
+                </script>
     </body>
 
 </html>
--- index.php
+++ index.php
@@ -8,27 +8,11 @@ if (isset($_GET['debug'])) {
 if (isset($_GET['pagina']) && $_GET['pagina'] == 'logout') {
     session_destroy();
     session_start();
-    header('location ?');
 }
   include_once 'lib/conexao.php';
-  //include_once 'lib/sql.php';
-  //include_once 'lib/auntenticar.php';
-
-//limpar sacola
-if(isset($_POST['limpar_sacola'])) {
-  unset($_session['sacola']);
-}
-
-//adicionar a sacola
-if (isset($_POST['adicionar_sacola'])) {
-  $_session['sacola'][] = $_GET['id'];
-}
-//remover a sacola
-if (isset($_POST['remover_sacola'])) {
-    unset($_SESSION['sacola'][$_POST['produto']]);
-}
-
-include 'home.php';
+  include_once 'lib/sql.php';
+  include_once 'lib/autenticar.php';
+  include_once 'home.php';
 // if (isset($_SESSION['autenticado'])) {
 //     if (isset($_GET['pagina'])) {
 //         //buscar no banco de dados se a página requisitada existe
