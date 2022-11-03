76d6023411aca566a6fcea6f5ea0544b2be47a74
jamesrobertojunglos <jamesrobertojunglos75@gmail.com>
2022-11-03 13:30:58 -0300

    Atulizei o Bootstrap para verão @5.2.2

--- home.php
+++ home.php
@@ -5,27 +5,32 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Bootstrap demo</title>
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet"
-            integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
-
-        <script src="https://code.jquery.com/jquery-3.6.1.min.js"
-            integrity="sha256-o88AwQnZB+VDvE9tvIXrMQaPlFFSUTR+nldQm1LuPXQ=" crossorigin="anonymous"></script>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet"
+            integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
     </head>
 
     <body>
-        <div class="container">
-            <div class="row">
-                <div class="col">Banner</div>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous">
+        </script>
+    </body>
+
+</html>
+
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col">Banner</div>
+        </div>
+        <div class="row">
+            <div class="col"><?php include 'menu_horizontal.php'?></div>
+        </div>
+        <div class="row">
+            <div class="col-3">
+                <?php include "menu_categorias.php" ; ?>
             </div>
-            <div class="row">
-                <div class="col"><?php include 'menu_horizontal.php'?></div>
-            </div>
-            <div class="row">
-                <div class="col-3">
-                    <?php include "menu_categorias.php" ; ?>
-                </div>
-                <div class="col-9">
-                    <?php
+            <div class="col-9">
+                <?php
                         if (isset($_GET['pagina'])) {
                         if ($_GET['pagina'] =='produtos') {
                             include 'produtos_home.php';
@@ -50,18 +55,18 @@
                           include "produtos_destaque.php" ; 
                         }
                     ?>
-                </div>
             </div>
         </div>
-        <div class="row" style="background-color:aqua">@meu_commerce 2022</div>
-        </div>
+    </div>
+    <div class="row" style="background-color:aqua">@meu_commerce 2022</div>
+    </div>
 
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js"
-            integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous">
-        </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous">
+    </script>
 
-        <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 
-    </body>
+</body>
 
 </html>
--- index.php
+++ index.php
@@ -34,33 +34,34 @@ if (isset($_GET['pagina']) && $_GET['pagina'] == 'logout') {
     
 
   include_once 'home.php';
-// if (isset($_SESSION['autenticado'])) {
-//     if (isset($_GET['pagina'])) {
-//         //buscar no banco de dados se a página requisitada existe
-//         $sql = "SELECT *
-//                     FROM paginas
-//                     WHERE id = :id
-//             ";
-//         $consulta = $conn->prepare($sql);
-//         $consulta->execute(['id' => $_GET['pagina']]);
-//         $linha = $consulta->fetch();
-//         if ($consulta->rowCount() > 0) {
-//             include 'menu.php';
-//             include $linha['src'];
-//         } else {
-//             include 'menu.php';
-//             include '404.php';
-//         }
-//     } else {
-//         include 'menu.php';
-//         include 'home.php';
-//     }
-// } else {
-//     include 'login.php';
-// }
+ //if (isset($_SESSION['autenticado'])) {
+    //if (isset($_GET['pagina'])) {
+        //buscar no banco de dados se a página requisitada existe
+      //$sql = "SELECT *
+                   //FROM paginas
+                    //WHERE id = :id          
+                    //";
+//$consulta = $conn->prepare($sql);
+       //$consulta->execute(['id' => $_GET['pagina']]);
+      //$linha = $consulta->fetch();
+         //if ($consulta->rowCount() > 0) {
+            //include 'menu.php';             
+            //include $linha['src'];
+    //} else {
+          //include 'menu.php';
+                    // include '404.php';
+        //}
+        //} else {
+         //include 'menu.php';
+         //include 'menu.php';
+        //nclude 'home.php';
+   //}
+//} else {
+    //include 'login.php';
+//}
 
-if (isset($SESSION['debug'])) {
-  if ($_SESSION['debug'] == true) {
-    include 'lib/debug.php';
-  }
-}
+//if (isset($SESSION['debug'])) {
+  //if ($_SESSION['debug'] == true) {
+    //include 'lib/debug.php';
+  ///}
+//}
